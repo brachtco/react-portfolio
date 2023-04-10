d96f467b44034f7795293d7cabde54232a3e0115
brachtco <brachtdanny@gmail.com>
2023-04-10 11:14:34 -0600

    Revise contact.js and footer.js

---
+++ my-react-portfolio/src/App.js
@@ -0,0 +1,38 @@
+//import logo from './logo.svg';
+//import './App.css';
+import React from 'react';
+import Navigation from './components/pages/Navigation'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import About from './components/pages/About';
+import Portfolio from './components/pages/Portfolio';
+import Contact from './components/pages/Contact';
+
+export default function App() {
+  return (
+  <p>Hello</p>
+
+  );
+}
+
+/*function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;*/
---
+++ my-react-portfolio/src/components/pages/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Contact() {
+  return (
+    <div>
+      <h1>Contact Page</h1>
+      <p>
+        Email: brachtdanny@gmail.com
+        Phone: 435.513.2938
+        GitHub: github.com/brachtco
+      </p>
+    </div>
+  );
+}
